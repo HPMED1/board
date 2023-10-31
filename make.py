a0b845eeedfa05404b4200dcacc9d4e713b380d6
HPMED1 <62830536+HPMED1@users.noreply.github.com>
2023-10-31 23:42:24 +0200

    gg

--- make.py
+++ make.py
@@ -6,7 +6,15 @@ htmlbody = (f"""<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="msapplication-TileImage" content="./prof.png">
+    <meta property="og:site_name" content="HPMED's board">
+    <meta property="og:title" content="I think i got something funny for you">
+    <meta property="og:image" itemprop="image" content="./prof.png">
+    <meta property="og:type" content="website" />
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:image:width" content="300">
+    <meta property="og:image:height" content="300">
+    <title>HPMED's board</title>
 </head>
 <body>
     <center>
@@ -22,4 +30,4 @@ with open(f'./{name}.html', 'w') as f:
 os.system("git add .")
 os.system("git commit -m gg")
 os.system("git push")
-console.print_markdown(f"https://hpmed1.github.io/board/{name}")
+console.print_step(f"https://hpmed1.github.io/board/{name}")
